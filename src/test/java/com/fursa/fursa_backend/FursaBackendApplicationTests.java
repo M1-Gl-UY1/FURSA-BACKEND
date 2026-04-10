d986f7e623bd46c151a3f57294ab8c9be6426482
emilekmdDev <emilekamdem.c@gmail.com>
2026-04-10 11:40:51 +0100

    init project

---
+++ src/test/java/com/fursa/fursa_backend/FursaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fursa.fursa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FursaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
