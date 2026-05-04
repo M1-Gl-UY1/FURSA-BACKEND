5d9fbcfaed49562f2aa0df44922cdaaf9a8f89a4
Scorpion_3.0 <tiomelajorel@gmail.com>
2026-05-04 09:27:00 +0100

    fix(security): import PreAuthorize manquant sur MarcheSecondaireController

--- src/main/java/com/fursa/fursa_backend/controller/MarcheSecondaireController.java
+++ src/main/java/com/fursa/fursa_backend/controller/MarcheSecondaireController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
